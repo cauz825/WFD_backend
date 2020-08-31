d46a0ec4b21eeb07b546484ab18a551bf56667f8
MCauz <michaelhcausey@gmail.com>
2020-08-31 10:07:47 -0400

    Monday morning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@ class UsersController < ApplicationController
     
     def index
         @users = User.all
-        render json: @users
     end
 
     def show
         @user = User.find(user.id)
-        render json: @user
     end
 
 end
