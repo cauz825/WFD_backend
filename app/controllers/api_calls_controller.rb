ba3e68aad7503095e3c9db6c31108a22fe13b456
MCauz <michaelhcausey@gmail.com>
2020-09-05 11:01:58 -0400

    results work

--- app/controllers/api_calls_controller.rb
+++ app/controllers/api_calls_controller.rb
@@ -1,8 +1,9 @@
 class ApiCallsController < ApplicationController
 
     def api_recipe_call
+        @user = User.find_by(id: params[:user_id])
         pantry = @user.ingredients
-        request_inv = pantry_query.join("%252C")
+        request_inv = pantry.join("%252C")
         byebug
         # response = JSON.parse(RestClient.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=#{usersPantry}",
         #   headers={
