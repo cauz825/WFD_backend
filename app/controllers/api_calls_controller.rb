1386dae7b0d9d16b463e950f701bf2546d64547e
MCauz <michaelhcausey@gmail.com>
2020-09-06 01:13:01 -0400

    api issue outstanding

--- app/controllers/api_calls_controller.rb
+++ app/controllers/api_calls_controller.rb
@@ -2,12 +2,15 @@ class ApiCallsController < ApplicationController
 
     def api_recipe_call
         @user = User.find_by(id: params[:user_id])
-        pantry = @user.ingredients
-        request_inv = pantry.join("%252C")
-        byebug
-        # response = JSON.parse(RestClient.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=#{usersPantry}",
-        #   headers={
-        #     })
-        render json: response
+        pantry = @user.ingredients.map{|ingr| ingr.name.downcase}
+        request_inv = pantry.join("%252C+")
+        # byebug
+        response = JSON.parse(RestClient.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=#{request_inv}",
+        headers:{
+        "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "X-RapidAPI-Key" => "995cb3893fmshafa470a32401ebep1da934jsnab94321dbf5d"
+        })
+        render json: {resp_back: response}
     end
+
 end
