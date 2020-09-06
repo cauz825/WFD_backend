5c796e8a89d4291959b296158cec3ff772103eb3
MCauz <michaelhcausey@gmail.com>
2020-09-06 13:20:30 -0400

    api problem resolved

--- app/controllers/api_calls_controller.rb
+++ app/controllers/api_calls_controller.rb
@@ -4,11 +4,11 @@ class ApiCallsController < ApplicationController
         @user = User.find_by(id: params[:user_id])
         pantry = @user.ingredients.map{|ingr| ingr.name.downcase}
         request_inv = pantry.join("%252C+")
-        # byebug
+        byebug
         response = JSON.parse(RestClient.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=#{request_inv}",
-        headers:{
-        "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        "X-RapidAPI-Key" => "995cb3893fmshafa470a32401ebep1da934jsnab94321dbf5d"
+        headers = {
+          "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+          "X-RapidAPI-Key" => "dfa93315fbmsh0da8826fc9ab800p1501fbjsn5c20c18a488e"
         })
         render json: {resp_back: response}
     end
